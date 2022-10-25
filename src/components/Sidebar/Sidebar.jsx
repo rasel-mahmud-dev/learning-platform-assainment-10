fd92d7c2cfdcd25acda79e957d8d7954a77cbca9
rasel-mahmud-dev <rasel.mahmud.dev@gmail.com>
2022-10-25 16:50:19 +0600

    added sidebar for filter course in courses page

---
+++ src/components/Sidebar/Sidebar.jsx
@@ -0,0 +1,16 @@
+import React from "react"
+import "./sidebar.css"
+const Sidebar = ({isOpen, children, onClose})=> {
+  
+  return (
+    <>
+      {isOpen &&<div className="sidebar-overlay" onClick={onClose} /> }
+      <div className="sidebar">
+        
+        {children}
+      
+      </div>
+    </>      
+    )
+}
+export default Sidebar
--- src/pages/Courses/Courses.jsx
+++ src/pages/Courses/Courses.jsx
@@ -1,17 +1,103 @@
 import React, { useContext, useEffect, useState } from "react";
 import AppContext from "../../context/AppContext.jsx";
 import Course from "../../components/Course/Course.jsx";
+import Sidebar from "../../components/Sidebar/Sidebar.jsx";
+import Rating from "../../components/Rating.jsx";
 
 const Courses = () => {
     const { state, setCourses } = useContext(AppContext);
-    console.log(state);
+    const [isOpenSidebar, setOpenSidebar] = useState(true);
+    const [filter, setFilter] = useState({
+        category: [],
+        ratings: [],
+        languange: "",
+        priceRange: [0, 100],
+        duration: 100,
+    });
+    const categories = [
+        { id: 1, name: "Web development" },
+        { id: 2, name: "App development" },
+        { id: 7, name: "Mechine Learning" },
+        { id: 12, name: "Cyber Security" },
+        { id: 21, name: "Ethical Hacking" },
+        { id: 3, name: "Graphich design" },
+        { id: 4, name: "Learning English" },
+        { id: 5, name: "Video Editing" },
+        { id: 6, name: "Logo Design" },
+    ];
 
     useEffect(() => {
         setCourses(fetchAllCourses());
     }, []);
 
+    function filterByCategoryHandler(category) {
+        let updateFilter = [...filter.category];
+        let existIndex = updateFilter.findIndex((cat) => cat.id === category.id);
+        if (existIndex !== -1) {
+            updateFilter.splice(existIndex, 1);
+        } else {
+            updateFilter.push(category);
+        }
+        // alert(JSON.stringify(updateFilter))
+        setFilter((prev) => ({
+            ...prev,
+            category: updateFilter,
+        }));
+    }
+    function filterByRatingHandler(rateNumber) {
+        alert("ASD");
+    }
     return (
         <div className="px-4">
+            <Sidebar isOpen={isOpenSidebar} onClose={() => setOpenSidebar(!isOpenSidebar)}>
+                <div className="p-5">
+                    <h1 className="font-bold text-lg text-base-500">Categories</h1>
+                    {filter.category.length ? (
+                        <div className="mt-2 flex flex-wrap gap-2">
+                            {filter.category.map((cat) => (
+                                <div className="badge">{cat.name}</div>
+                            ))}
+                        </div>
+                    ) : (
+                        ""
+                    )}
+                    <div className="mt-1">
+                        {categories.map((category) => (
+                            <div className="text-base-700 flex items-center gap-x-2 cursor-pointer">
+                                <input
+                                    onChange={() => filterByCategoryHandler(category)}
+                                    type="checkbox"
+                                    id={category.name}
+                                    className="checkbox !rounded checkbox-xs checkbox-primary "
+                                    checked={!!filter.category.find((cat) => cat.id === category.id)}
+                                />
+                                <label htmlFor={category.name} className="py-1 font-medium">
+                                    {category.name}
+                                </label>
+                            </div>
+                        ))}
+                    </div>
+
+                    <h1 className="font-bold text-lg text-base-500 mt-3">Rating</h1>
+                    <div className="">
+                        {new Array(5).fill(0).map((_, index) => (
+                            <div className="text-base-700 flex items-center gap-x-2 cursor-pointer">
+                                <input
+                                    onChange={() => filterByRatingHandler(index + 1)}
+                                    type="checkbox"
+                                    id={"rate-" + index}
+                                    className="checkbox !rounded checkbox-xs checkbox-primary "
+                                    checked={filter.ratings.includes(index + 1)}
+                                />
+                                <label htmlFor={"rate-" + index} className="py-1 font-medium flex items-center gap-x-2">
+                                    {index + 1}
+                                    <Rating rate={index + 1} id={"rate-" + index} />
+                                </label>
+                            </div>
+                        ))}
+                    </div>
+                </div>
+            </Sidebar>
             <div className="content">
                 <div className="ml-4">
                     <h1 className="text-xl font-bold mt-4 mb-3">Courses </h1>
